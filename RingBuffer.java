0a267d8c8b2c7082cbaf91fe94231e3bb7bfc73d
iifruitshake <92800647+iifruitshake@users.noreply.github.com>
2021-11-11 08:24:23 -0600

    Update RingBuffer.java

--- RingBuffer.java
+++ RingBuffer.java
@@ -19,6 +19,8 @@
 
 public class RingBuffer {
     // YOUR INSTANCE VARIABLES HERE
+	private int[] ring;
+	int frnt,back;
     // You are creating a circular queue.
     // Look up how to create a cirucular queue using an array.
 
@@ -27,41 +29,65 @@ public class RingBuffer {
     // creates an empty ring buffer with the specified capacity
     public RingBuffer(int capacity) {
         // YOUR CODE HERE
+    	ring = new int[capacity];
+    	frnt= 0;
+    	back= 0 ;
     }
 
     // return the capacity of this ring buffer
     public int capacity() {
         // YOUR CODE HERE
+    	return ring.length;
     }
 
     // return number of items currently in this ring buffer
     public int size() {
         // YOUR CODE HERE
+    	int temp=0;
+    	for(int i=0;i<ring.length;i++)
+    	{
+    		if(ring[i]!=0)
+    			temp++;
+    	}
+    	return temp;
     }
 
     // is this ring buffer empty (size equals zero)?
     public boolean isEmpty() {
         // YOUR CODE HERE
+    	return size() == 0;
     }
 
     // is this ring buffer full (size equals capacity)?
     public boolean isFull() {
         // YOUR CODE HERE
+    	return size() == capacity();
     }
 
     // adds item x to the end of this ring buffer
     public void enqueue(double x) {
         // YOUR CODE HERE
+    	if(isFull())
+    	{
+    		return;
+    	}
+    	if(back+1 == size() && frnt!=0)
+    	{
+    		
+    	}
+    	
     }
 
     // deletes and returns the item at the front of this ring buffer
     public double dequeue() {
         // YOUR CODE HERE
+    	return 0.0;
     }
 
     // returns the item at the front of this ring buffer
     public double peek() {
         // YOUR CODE HERE
+    	return 0.0;
     }
 
     // tests and calls every instance method in this class
@@ -70,3 +96,4 @@ public class RingBuffer {
     }
 
 }
+
