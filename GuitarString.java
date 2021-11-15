7189c0ecd315b44642d8bfca33193b7076ec7f9f
Clamden <92802171+Clamden@users.noreply.github.com>
2021-11-15 09:09:31 -0600

    Update GuitarString.java

--- GuitarString.java
+++ GuitarString.java
@@ -20,11 +20,15 @@
 
 public class GuitarString {
     // YOUR INSTANCE VARIABLES HERE
-
+	private RingBuffer ring;
+	private int rsize,ttic;
     // creates a guitar string of the specified frequency,
     // using sampling rate of 44,100
     public GuitarString(double frequency) {
         // YOUR CODE HERE
+    	int a = (int)Math.ceil(44100/frequency);
+    	ring = new RingBuffer(a);
+    	rsize = a;
     }
 
     // creates a guitar string whose size and initial values are given by
@@ -36,6 +40,7 @@ public class GuitarString {
     // returns the number of samples in the ring buffer
     public int length() {
         // YOUR CODE HERE
+    	return ring.size();
     }
 
     // plucks the guitar string (by replacing the buffer with white noise)
@@ -46,13 +51,23 @@ public class GuitarString {
     // advances the Karplus-Strong simulation one time step
     public void tic() {
         // YOUR CODE HERE
+    	double x = ring.peek();
+    	ring.dequeue();
+    	double y = ring.peek();
+    	double xy = (x+y)/(2)*(.994);
+    	ring.enqueue(xy);
     }
 
     // returns the current sample
     public double sample() {
         // YOUR CODE HERE
+    	return ring.peek();
     }
-
+    
+    public int Time() {
+    	return ttic;
+    }
+    
 
     // tests and calls every constructor and instance method in this class
     public static void main(String[] args) {
